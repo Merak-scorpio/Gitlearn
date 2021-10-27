e0e7b5d35a7c8679084e15a2f0a2998dd374a349
Merak-scorpio <2663929671@qq.com>
2021-10-28 00:53:06 +0800

    v1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
+	string str = "hello world !";
 	cout << "hello world" << endl;
-
+	cout << str << endl;
+	
 	return 0;
 }
